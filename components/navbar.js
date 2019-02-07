cbd02076542ae5e08365d17246bdd27613beebfc
Thorben Bender <bender.thorben0@gmail.com>
2019-02-07 15:31:10 +0100

    working on navbar.js and fixing css

--- components/navbar.js
+++ components/navbar.js
@@ -2,14 +2,26 @@ class Navbar {
     constructor(nav) {
         this.element = nav;
         this.button = document.querySelector('.nav-open-close');
-        this.button.addEventListener('click', () => this.showNav());
         this.menu = document.querySelector('.main-container');
+        this.button.addEventListener('click', () => this.closeOrOpen());
+        this.menu.addEventListener('mouseout', () => this.hideNav());
+    }
+    closeOrOpen(){
+        if (this.element.classList.contains('nav-show')){
+            this.hideNav();
+        }
+        else{
+            this.showNav();
+        }
     }
     showNav() {
-        this.element.classList.toggle('nav-show');
-        this.menu.classList.toggle('header-visible');
-
-
+        this.element.classList.add('nav-show');
+        this.menu.classList.add('header-visible');
+        event.stopImmediatePropagation();
+    }
+    hideNav(){
+        this.element.classList.remove('nav-show');
+        this.menu.classList.remove('header-visible');
     }
 }
 
